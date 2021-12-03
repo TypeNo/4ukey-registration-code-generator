3203104929d7565725dd734006a95f8079e935e4
TypeNo <70062162+TypeNo@users.noreply.github.com>
2021-12-04 02:29:14 +0800

    Add files via upload

--- Parking slot detection and 2d map/Run.py
+++ Parking slot detection and 2d map/Run.py
@@ -1,5 +1,9 @@
 import os
+import detector
 import map
 
+
 #os.system('map.py')
+detector
 map
+
--- Parking slot detection and 2d map/map.py
+++ Parking slot detection and 2d map/map.py
@@ -22,38 +22,41 @@ if(zx==True):
         csvr=csv.reader(inf)
         zlist=list(csvr)
 
-zlist=[[int(float(j)) for j in i]for i in zlist]
-for i in range(len(zlist)):
-    cv2.rectangle(img,(zlist[i][0],zlist[i][1]),(zlist[i][2],zlist[i][3]),(0,255,0),-1)
+    zlist=[[int(float(j)) for j in i]for i in zlist]
+    for i in range(len(zlist)):
+        cv2.rectangle(img,(zlist[i][0],zlist[i][1]),(zlist[i][2],zlist[i][3]),(0,255,0),-1)
     
-    y=min(i[1] for i in zlist)
-    if(y<min(i[3] for i in zlist)):
-        x=min(i[0] for i in zlist if i[1]<min(i[3] for i in zlist))
-        z=min(i[2] for i in zlist if i[1]<min(i[3] for i in zlist))
-    else:
-        x=min(i[0] for i in zlist if i[1]>min(i[3] for i in zlist))
-        z=min(i[2] for i in zlist if i[1]<min(i[3] for i in zlist))
-    q=[x,z]
-    r=statistics.mean(q)
+        y=min(i[1] for i in zlist)
+        if(y<min(i[3] for i in zlist)):
+            x=min(i[0] for i in zlist if i[1]<min(i[3] for i in zlist))
+            z=min(i[2] for i in zlist if i[1]<min(i[3] for i in zlist))
+            a=min(i[3] for i in zlist if i[1]<min(i[3] for i in zlist))
+        else:
+            x=min(i[0] for i in zlist if i[1]<min(i[3] for i in zlist))
+            z=min(i[2] for i in zlist if i[1]<min(i[3] for i in zlist))
+            a=min(i[3] for i in zlist if i[1]<min(i[3] for i in zlist))
+        q=[x,z]
+        r=statistics.mean(q)
+        b=min(i[1] for i in rlist)+min(i[3] for i in rlist)
     #r=[min(i[1] for i in zlist),min(i[3]for i in zlist)]
-    if(y<min(i[3] for i in zlist)):
+        if(y<b):
         
-        cv2.line(img,(10,20),(10,y-50),(255,255,255),1)
-        cv2.line(img,(10,y-50),(int(r),y-50),(255,255,255),1)
-        cv2.line(img,(int(r),y-50),(int(r),y),(255,255,255),1)
-    else:
-        cv2.line(img,(10,20),(10,y+50),(255,255,255),1)
-        cv2.line(img,(10,y+50),(int(r),y+50),(255,255,255),1)
-        cv2.line(img,int(r),y+50),(int(r,y),(255,255,255),1)
+            cv2.line(img,(10,20),(10,y-50),(255,255,255),1)
+            cv2.line(img,(10,y-50),(int(r),y-50),(255,255,255),1)
+            cv2.line(img,(int(r),y-50),(int(r),y),(255,255,255),1)
+        else:
+            cv2.line(img,(10,20),(10,a+50),(255,255,255),1)
+            cv2.line(img,(10,a+50),(int(r),a+50),(255,255,255),1)
+            cv2.line(img,(int(r),a+50),(int(r),a),(255,255,255),1)
 yx=os.path.isfile("data/y.csv")
 if(yx==True):
     with open('data/y.csv','r',newline='')as inf:
         csvr=csv.reader(inf)
         ylist=list(csvr)
+    ylist=[[int(float(j))for j in i]for i in ylist]
+    for i in range(len(ylist)):
+        cv2.rectangle(img,(ylist[i][0],ylist[i][1]),(ylist[i][2],ylist[i][3]),(0,0,255),-1)
 
 
-ylist=[[int(float(j))for j in i]for i in ylist]
-for i in range(len(ylist)):
-    cv2.rectangle(img,(ylist[i][0],ylist[i][1]),(ylist[i][2],ylist[i][3]),(0,0,255),-1)
 cv2.imshow('paper',img)
 cv2.waitKey(0)
